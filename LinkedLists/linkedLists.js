debc84c10d785e29411de68652352538c825216e
Alamin Hossain <alaminhossain@Alamins-MBP.home>
2017-04-23 18:33:19 -0500

    added tail

--- LinkedLists/linkedLists.js
+++ LinkedLists/linkedLists.js
@@ -11,17 +11,40 @@ function Node(value, next, prev){
 
 LinkedList.prototype.addToHead = function(value){
   var newNode = new Node(value, this.head, null);
-  if(this.head) {this.head.prev = newNode;
-    console.log('hit')
-  }
-  else { this.tail = newNode;
-    console.log('hit2')
-  }
+  if(this.head) this.head.prev = newNode;
+  else this.tail = newNode;
   this.head = newNode;
+}
+LinkedList.prototype.addToTail = function(value) {
+  var newNode = new Node(value, null, this.tail)
+  if(this.tail) this.tail.next = newNode;
+  else this.head = newNode
+  this.tail = newNode;
 }
 
 var ll = new LinkedList();
-ll.addToHead(10);
-ll.addToHead(10);
-ll.addToHead(20);
+
+
+ll.addToHead(100);
+ll.addToHead(200);
+ll.addToTail(300);
+// ll.addToHead(20);
 console.log(ll)
+
+// function LinkedList(){
+//   this.head = null;
+//   this.tail = null;
+// }
+//
+// function Node (value, next,prev){
+//   this.value = value;
+//   this.next = next;
+//   this.prev = prev;
+// }
+//
+// LinkedList.prototype.addToHead = (value) => {
+//   var newNode = new Node(value, this.head, null)
+//   if(this.head) this.head.prev = newNode;
+//   else this.tail = newNode;
+//   this.head = newNode;
+// }
