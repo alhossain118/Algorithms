b6c242d7cc3769c1f20367696481b1f85ce99b0e
Alamin Hossain <alaminhossain@Alamins-MacBook-Pro.local>
2017-05-16 14:53:17 -0500

    added breadthFirstTraversal method

--- Binary-Search-Trees/algo.js
+++ Binary-Search-Trees/algo.js
@@ -1,32 +1,50 @@
-function BST(value){
-  this.value = value;
-  this.left = null;
-  this.right = null;
+function BST(value) {
+    this.value = value;
+    this.right = null;
+    this.left = null;
 }
 
-BST.prototype.insert = function(value){
 
-  if(value <= this.value){
-    if(!this.left) this.left = new BST(value);
-    else this.left.insert(value);
-  }
-  else if(value >= this.value){
-    if(!this.right) this.right = new BST(value);
-    else this.right.insert(value);
-  }
+BST.prototype.insert = function(value) {
 
+    if (value <= this.value) {
+        if (!this.left) this.left = new BST(value);
+        else this.left.insert(value);
+    } else if (value >= this.value) {
+        if (!this.right) this.right = new BST(value);
+        else this.right.insert(value);
+    }
 }
 
-BST.prototype.contains = function(value){
-  if(value = this.value) return true;
-  else if(value < this.value){
-    if(!this.left) return false
-    else return this.left.contains(value)
-  }
-  else if(value > this.value){
-    if(!this.right) return false;
-    else return this.right.contains(value)
-  }
+
+
+BST.prototype.contains = function(value) {
+    if (value === this.value) return true;
+    else if (value < this.value) {
+        if (!this.left) return false
+        else return this.left.contains(value)
+    } else if (value > this.value) {
+        if (!this.right) return false;
+        else return this.right.contains(value)
+    }
+}
+
+BST.prototype.depthFirstTraversal = function(interatorFunc, order) {
+    if (order === 'pre-order') interatorFunc(this.value)
+    if (this.left) this.left.depthFirstTraversal(interatorFunc, order);
+    if (order === 'in-order') interatorFunc(this.value);
+    if (this.right) this.right.depthFirstTraversal(interatorFunc, order);
+    if (order === 'post-order') interatorFunc(this.value)
+}
+
+BST.prototype.breadthFirstTraversal = function(interatorFunc) {
+    var queue = [this];
+    while (queue.length) {
+        var treeNode = queue.shift();
+        interatorFunc(treeNode);
+        if (treeNode.left) queue.push(treeNode.left);
+        if (treeNode.right) queue.push(treeNode.right)
+    }
 
 }
 
@@ -43,4 +61,11 @@ bst.insert(85)
 bst.insert(105)
 bst.insert(10)
 
-console.log(bst.right.left.left);
+bst.depthFirstTraversal(log, 'pre-order')
+
+function log(value) {
+    console.log(value);
+}
+
+
+// console.log(bst.right.left.left);
